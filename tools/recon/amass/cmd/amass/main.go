6d05d1a98c8e303c48929e96b6887adca04bdf6a
sudoadminservices <77534791+sudoadminservices@users.noreply.github.com>
2021-01-16 20:24:30 -0500

    Add files via upload

---
+++ tools/recon/amass/cmd/amass/main.go
@@ -0,0 +1,451 @@
+// Copyright © by Jeff Foley 2017-2020. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+// SPDX-License-Identifier: Apache-2.0
+
+// In-depth Attack Surface Mapping and Asset Discovery
+//  +----------------------------------------------------------------------------+
+//  | ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░  OWASP Amass  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ |
+//  +----------------------------------------------------------------------------+
+//  |      .+++:.            :                             .+++.                 |
+//  |    +W@@@@@@8        &+W@#               o8W8:      +W@@@@@@#.   oW@@@W#+   |
+//  |   &@#+   .o@##.    .@@@o@W.o@@o       :@@#&W8o    .@#:  .:oW+  .@#+++&#&   |
+//  |  +@&        &@&     #@8 +@W@&8@+     :@W.   +@8   +@:          .@8         |
+//  |  8@          @@     8@o  8@8  WW    .@W      W@+  .@W.          o@#:       |
+//  |  WW          &@o    &@:  o@+  o@+   #@.      8@o   +W@#+.        +W@8:     |
+//  |  #@          :@W    &@+  &@+   @8  :@o       o@o     oW@@W+        oW@8    |
+//  |  o@+          @@&   &@+  &@+   #@  &@.      .W@W       .+#@&         o@W.  |
+//  |   WW         +@W@8. &@+  :&    o@+ #@      :@W&@&         &@:  ..     :@o  |
+//  |   :@W:      o@# +Wo &@+        :W: +@W&o++o@W. &@&  8@#o+&@W.  #@:    o@+  |
+//  |    :W@@WWWW@@8       +              :&W@@@@&    &W  .o#@@W&.   :W@WWW@@&   |
+//  |      +o&&&&+.                                                    +oooo.    |
+//  +----------------------------------------------------------------------------+
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"path"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/OWASP/Amass/v3/config"
+	"github.com/OWASP/Amass/v3/datasrcs"
+	"github.com/OWASP/Amass/v3/format"
+	"github.com/OWASP/Amass/v3/graph"
+	amassnet "github.com/OWASP/Amass/v3/net"
+	"github.com/OWASP/Amass/v3/requests"
+	"github.com/OWASP/Amass/v3/stringfilter"
+	"github.com/OWASP/Amass/v3/systems"
+	"github.com/caffix/eventbus"
+	"github.com/fatih/color"
+)
+
+const (
+	mainUsageMsg         = "intel|enum|viz|track|db|dns [options]"
+	exampleConfigFileURL = "https://github.com/OWASP/Amass/blob/master/examples/config.ini"
+	userGuideURL         = "https://github.com/OWASP/Amass/blob/master/doc/user_guide.md"
+	tutorialURL          = "https://github.com/OWASP/Amass/blob/master/doc/tutorial.md"
+)
+
+var (
+	// Colors used to ease the reading of program output
+	g      = color.New(color.FgHiGreen)
+	r      = color.New(color.FgHiRed)
+	b      = color.New(color.FgHiBlue)
+	fgR    = color.New(color.FgRed)
+	fgY    = color.New(color.FgYellow)
+	yellow = color.New(color.FgHiYellow).SprintFunc()
+	green  = color.New(color.FgHiGreen).SprintFunc()
+	blue   = color.New(color.FgHiBlue).SprintFunc()
+	red    = color.New(color.FgHiRed).SprintFunc()
+)
+
+func commandUsage(msg string, cmdFlagSet *flag.FlagSet, errBuf *bytes.Buffer) {
+	format.PrintBanner()
+	g.Fprintf(color.Error, "Usage: %s %s\n\n", path.Base(os.Args[0]), msg)
+	cmdFlagSet.PrintDefaults()
+	g.Fprintln(color.Error, errBuf.String())
+
+	if msg == mainUsageMsg {
+		g.Fprintf(color.Error, "\nSubcommands: \n\n")
+		g.Fprintf(color.Error, "\t%-11s - Discover targets for enumerations\n", "amass intel")
+		g.Fprintf(color.Error, "\t%-11s - Perform enumerations and network mapping\n", "amass enum")
+		g.Fprintf(color.Error, "\t%-11s - Visualize enumeration results\n", "amass viz")
+		g.Fprintf(color.Error, "\t%-11s - Track differences between enumerations\n", "amass track")
+		g.Fprintf(color.Error, "\t%-11s - Manipulate the Amass graph database\n", "amass db")
+		g.Fprintf(color.Error, "\t%-11s - Resolve DNS names at high performance\n\n", "amass dns")
+	}
+
+	g.Fprintf(color.Error, "The user's guide can be found here: \n%s\n\n", userGuideURL)
+	g.Fprintf(color.Error, "An example configuration file can be found here: \n%s\n\n", exampleConfigFileURL)
+	g.Fprintf(color.Error, "The Amass tutorial can be found here: \n%s\n\n", tutorialURL)
+}
+
+func main() {
+	var version, help1, help2 bool
+	mainFlagSet := flag.NewFlagSet("amass", flag.ContinueOnError)
+
+	defaultBuf := new(bytes.Buffer)
+	mainFlagSet.SetOutput(defaultBuf)
+
+	mainFlagSet.BoolVar(&help1, "h", false, "Show the program usage message")
+	mainFlagSet.BoolVar(&help2, "help", false, "Show the program usage message")
+	mainFlagSet.BoolVar(&version, "version", false, "Print the version number of this Amass binary")
+
+	if len(os.Args) < 2 {
+		commandUsage(mainUsageMsg, mainFlagSet, defaultBuf)
+		return
+	}
+	if err := mainFlagSet.Parse(os.Args[1:]); err != nil {
+		r.Fprintf(color.Error, "%v\n", err)
+		os.Exit(1)
+	}
+	if help1 || help2 {
+		commandUsage(mainUsageMsg, mainFlagSet, defaultBuf)
+		return
+	}
+	if version {
+		fmt.Fprintf(color.Error, "%s\n", format.Version)
+		return
+	}
+
+	switch os.Args[1] {
+	case "db":
+		runDBCommand(os.Args[2:])
+	case "dns":
+		runDNSCommand(os.Args[2:])
+	case "enum":
+		runEnumCommand(os.Args[2:])
+	case "intel":
+		runIntelCommand(os.Args[2:])
+	case "track":
+		runTrackCommand(os.Args[2:])
+	case "viz":
+		runVizCommand(os.Args[2:])
+	default:
+		commandUsage(mainUsageMsg, mainFlagSet, defaultBuf)
+		os.Exit(1)
+	}
+}
+
+// GetAllSourceInfo returns the output for the 'list' flag.
+func GetAllSourceInfo(cfg *config.Config) []string {
+	var names []string
+
+	if cfg == nil {
+		cfg = config.NewConfig()
+	}
+
+	sys, err := systems.NewLocalSystem(cfg)
+	if err != nil {
+		return names
+	}
+	srcs := datasrcs.SelectedDataSources(cfg, datasrcs.GetAllSources(sys))
+	sys.SetDataSources(srcs)
+
+	names = append(names, fmt.Sprintf("%-35s%-35s%s", blue("Data Source"), blue("| Type"), blue("| Available")))
+	var line string
+	for i := 0; i < 8; i++ {
+		line += blue("----------")
+	}
+	names = append(names, line)
+
+	available := sys.DataSources()
+	for _, src := range srcs {
+		var avail string
+
+		for _, a := range available {
+			if src.String() == a.String() {
+				avail = "*"
+				break
+			}
+		}
+
+		names = append(names, fmt.Sprintf("%-35s  %-35s  %s",
+			green(src.String()), yellow(src.Description()), yellow(avail)))
+	}
+
+	sys.Shutdown()
+	return names
+}
+
+func createOutputDirectory(cfg *config.Config) {
+	// Prepare output file paths
+	dir := config.OutputDirectory(cfg.Dir)
+	if dir == "" {
+		r.Fprintln(color.Error, "Failed to obtain the output directory")
+		os.Exit(1)
+	}
+	// If the directory does not yet exist, create it
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		r.Fprintf(color.Error, "Failed to create the directory: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func generateCategoryMap(sys systems.System) map[string][]string {
+	catToSources := make(map[string][]string)
+
+	for _, src := range sys.DataSources() {
+		t := src.Description()
+
+		catToSources[t] = append(catToSources[t], src.String())
+	}
+
+	return catToSources
+}
+
+func expandCategoryNames(names []string, categories map[string][]string) []string {
+	var newnames []string
+
+	for _, name := range names {
+		if _, found := categories[name]; found {
+			newnames = append(newnames, categories[name]...)
+			continue
+		}
+
+		newnames = append(newnames, name)
+	}
+
+	return newnames
+}
+
+func openGraphDatabase(dir string, cfg *config.Config) *graph.Graph {
+	for _, db := range cfg.GraphDBs {
+		if !db.Primary {
+			continue
+		}
+
+		cayley := graph.NewCayleyGraph(db.System, db.URL, db.Options)
+		if cayley == nil {
+			return nil
+		}
+
+		g := graph.NewGraph(cayley)
+		if g == nil {
+			return nil
+		}
+
+		return g
+	}
+
+	if db := cfg.LocalDatabaseSettings(cfg.GraphDBs); db != nil {
+		db.Options = ""
+
+		cayley := graph.NewCayleyGraph(db.System, config.OutputDirectory(dir), db.Options)
+		if cayley == nil {
+			return nil
+		}
+
+		if g := graph.NewGraph(cayley); g != nil {
+			return g
+		}
+	}
+
+	return nil
+}
+
+func orderedEvents(events []string, db *graph.Graph) ([]string, []time.Time, []time.Time) {
+	sort.Slice(events, func(i, j int) bool {
+		var less bool
+
+		e1, l1 := db.EventDateRange(events[i])
+		e2, l2 := db.EventDateRange(events[j])
+		if l2.After(l1) || e1.Before(e2) {
+			less = true
+		}
+
+		return less
+	})
+
+	var earliest, latest []time.Time
+	for _, event := range events {
+		e, l := db.EventDateRange(event)
+
+		earliest = append(earliest, e)
+		latest = append(latest, l)
+	}
+
+	return events, earliest, latest
+}
+
+// Obtain the enumeration IDs that include the provided domain
+func eventUUIDs(domains []string, db *graph.Graph) []string {
+	var uuids []string
+
+	for _, id := range db.EventList() {
+		if len(domains) == 0 {
+			uuids = append(uuids, id)
+			continue
+		}
+
+		var found bool
+		surface := db.EventDomains(id)
+		for _, domain := range surface {
+			if domainNameInScope(domain, domains) {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			uuids = append(uuids, id)
+		}
+	}
+
+	return uuids
+}
+
+func memGraphForScope(domains []string, from *graph.Graph) (*graph.Graph, error) {
+	db := graph.NewGraph(graph.NewCayleyGraphMemory())
+	if db == nil {
+		return nil, errors.New("Failed to create the in-memory graph database")
+	}
+
+	var err error
+	// Migrate the event data into the in-memory graph database
+	if len(domains) == 0 {
+		err = from.MigrateEvents(db)
+	} else {
+		err = from.MigrateEventsInScope(db, domains)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("Failed to move the data into the in-memory graph database: %v", err)
+	}
+
+	return db, nil
+}
+
+func getEventOutput(uuids []string, asninfo bool, db *graph.Graph, cache *amassnet.ASNCache) []*requests.Output {
+	var output []*requests.Output
+	filter := stringfilter.NewStringFilter()
+
+	for i := len(uuids) - 1; i >= 0; i-- {
+		output = append(output, db.EventOutput(uuids[i], filter, asninfo, cache)...)
+	}
+
+	return output
+}
+
+func domainNameInScope(name string, scope []string) bool {
+	var discovered bool
+
+	n := strings.ToLower(strings.TrimSpace(name))
+	for _, d := range scope {
+		d = strings.ToLower(d)
+
+		if n == d || strings.HasSuffix(n, "."+d) {
+			discovered = true
+			break
+		}
+	}
+
+	return discovered
+}
+
+func healASInfo(uuids []string, db *graph.Graph) bool {
+	cfg := config.NewConfig()
+	cfg.LocalDatabase = false
+
+	sys, err := systems.NewLocalSystem(cfg)
+	if err != nil {
+		return false
+	}
+	sys.SetDataSources(datasrcs.GetAllSources(sys))
+	defer sys.Shutdown()
+
+	cache := sys.Cache()
+	for _, g := range sys.GraphDatabases() {
+		g.ASNCacheFill(cache)
+	}
+
+	bus := eventbus.NewEventBus()
+	bus.Subscribe(requests.NewASNTopic, cache.Update)
+	defer bus.Unsubscribe(requests.NewASNTopic, cache.Update)
+	ctx := context.WithValue(context.Background(), requests.ContextConfig, cfg)
+	ctx = context.WithValue(ctx, requests.ContextEventBus, bus)
+
+	var updated bool
+	for _, uuid := range uuids {
+		for _, out := range db.EventOutput(uuid, nil, false, cache) {
+			for _, a := range out.Addresses {
+				if r := cache.AddrSearch(a.Address.String()); r != nil {
+					db.InsertInfrastructure(r.ASN, r.Description, r.Address, r.Prefix, r.Source, r.Tag, uuid)
+					continue
+				}
+
+				for _, src := range sys.DataSources() {
+					src.Request(ctx, &requests.ASNRequest{Address: a.Address.String()})
+				}
+
+				for i := 0; i < 30; i++ {
+					if cache.AddrSearch(a.Address.String()) != nil {
+						break
+					}
+					time.Sleep(time.Second)
+				}
+
+				if r := cache.AddrSearch(a.Address.String()); r != nil {
+					db.InsertInfrastructure(r.ASN, r.Description, r.Address, r.Prefix, r.Source, r.Tag, uuid)
+				}
+
+				updated = true
+			}
+		}
+	}
+
+	return updated
+}
+
+func assignNetInterface(iface *net.Interface) error {
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return fmt.Errorf("Network interface '%s' has no assigned addresses", iface.Name)
+	}
+
+	var best net.Addr
+	for _, addr := range addrs {
+		if a, ok := addr.(*net.IPNet); ok {
+			if best == nil {
+				best = a
+			}
+			if amassnet.IsIPv4(a.IP) {
+				best = a
+				break
+			}
+		}
+	}
+
+	if best == nil {
+		return fmt.Errorf("Network interface '%s' does not have assigned IP addresses", iface.Name)
+	}
+
+	amassnet.LocalAddr = best
+	return nil
+}
+
+func cacheWithData() *amassnet.ASNCache {
+	ranges, err := config.GetIP2ASNData()
+	if err != nil {
+		return nil
+	}
+
+	cache := amassnet.NewASNCache()
+	for _, r := range ranges {
+		cache.Update(&requests.ASNRequest{
+			Address:     r.FirstIP.String(),
+			ASN:         r.ASN,
+			CC:          r.CC,
+			Prefix:      amassnet.Range2CIDR(r.FirstIP, r.LastIP).String(),
+			Description: r.Description,
+		})
+	}
+
+	return cache
+}
---
+++ tools/recon/amass/config/config.go
@@ -0,0 +1,382 @@
+// Copyright 2017-2020 Jeff Foley. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+
+package config
+
+import (
+	"bufio"
+	"compress/gzip"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+
+	_ "github.com/OWASP/Amass/v3/config/statik" // The content being embedded into the binary
+	amasshttp "github.com/OWASP/Amass/v3/net/http"
+	"github.com/OWASP/Amass/v3/wordlist"
+	"github.com/caffix/stringset"
+	"github.com/go-ini/ini"
+	"github.com/google/uuid"
+	"github.com/rakyll/statik/fs"
+)
+
+const (
+	outputDirectoryName = "amass"
+)
+
+var (
+	// StatikFS is the ./resources project directory embedded into the binary.
+	StatikFS http.FileSystem
+	fsOnce   sync.Once
+)
+
+func openTheFS() {
+	StatikFS, _ = fs.New()
+}
+
+// Updater allows an object to implement a method that updates a configuration.
+type Updater interface {
+	OverrideConfig(*Config) error
+}
+
+// Config passes along Amass configuration settings and options.
+type Config struct {
+	sync.Mutex
+
+	// A Universally Unique Identifier (UUID) for the enumeration
+	UUID uuid.UUID
+
+	// Logger for error messages
+	Log *log.Logger
+
+	// The directory that stores the bolt db and other files created
+	Dir string `ini:"output_directory"`
+
+	// Alternative directory for scripts provided by the user
+	ScriptsDirectory string `ini:"scripts_directory"`
+
+	// Use a local graph database
+	LocalDatabase bool
+
+	// The graph databases used by the system / enumerations
+	GraphDBs []*Database
+
+	// The maximum number of concurrent DNS queries
+	MaxDNSQueries int `ini:"maximum_dns_queries"`
+
+	// Names provided to seed the enumeration
+	ProvidedNames []string
+
+	// The IP addresses specified as in scope
+	Addresses []net.IP
+
+	// CIDR that is in scope
+	CIDRs []*net.IPNet
+
+	// ASNs specified as in scope
+	ASNs []int
+
+	// The ports that will be checked for certificates
+	Ports []int
+
+	// The list of words to use when generating names
+	Wordlist []string
+
+	// Will the enumeration including brute forcing techniques
+	BruteForcing bool
+
+	// Will recursive brute forcing be performed?
+	Recursive bool
+
+	// Minimum number of subdomain discoveries before performing recursive brute forcing
+	MinForRecursive int
+
+	// Will discovered subdomain name alterations be generated?
+	Alterations    bool
+	FlipWords      bool
+	FlipNumbers    bool
+	AddWords       bool
+	AddNumbers     bool
+	MinForWordFlip int
+	EditDistance   int
+	AltWordlist    []string
+
+	// Only access the data sources for names and return results?
+	Passive bool
+
+	// Determines if zone transfers will be attempted
+	Active bool
+
+	// A blacklist of subdomain names that will not be investigated
+	Blacklist []string
+
+	// A list of data sources that should not be utilized
+	SourceFilter struct {
+		Include bool // true = include, false = exclude
+		Sources []string
+	}
+
+	// The minimum number of minutes that data source responses will be reused
+	MinimumTTL int
+
+	// Type of DNS records to query for
+	RecordTypes []string
+
+	// Resolver settings
+	Resolvers           []string
+	MonitorResolverRate bool
+
+	// Option for verbose logging and output
+	Verbose bool
+
+	// The root domain names that the enumeration will target
+	domains []string
+
+	// The regular expressions for the root domains added to the enumeration
+	regexps map[string]*regexp.Regexp
+
+	// The data source configurations
+	datasrcConfigs map[string]*DataSourceConfig
+}
+
+// NewConfig returns a default configuration object.
+func NewConfig() *Config {
+	c := &Config{
+		UUID:                uuid.New(),
+		Log:                 log.New(ioutil.Discard, "", 0),
+		Ports:               []int{443},
+		MinForRecursive:     1,
+		Resolvers:           DefaultBaselineResolvers,
+		MonitorResolverRate: true,
+		LocalDatabase:       true,
+		// The following is enum-only, but intel will just ignore them anyway
+		Alterations:    true,
+		FlipWords:      true,
+		FlipNumbers:    true,
+		AddWords:       true,
+		AddNumbers:     true,
+		MinForWordFlip: 2,
+		EditDistance:   1,
+		Recursive:      true,
+	}
+
+	c.calcDNSQueriesMax()
+	return c
+}
+
+// UpdateConfig allows the provided Updater to update the current configuration.
+func (c *Config) UpdateConfig(update Updater) error {
+	return update.OverrideConfig(c)
+}
+
+// CheckSettings runs some sanity checks on the configuration options selected.
+func (c *Config) CheckSettings() error {
+	var err error
+
+	if c.BruteForcing {
+		if c.Passive {
+			return errors.New("Brute forcing cannot be performed without DNS resolution")
+		} else if len(c.Wordlist) == 0 {
+			c.Wordlist, err = getWordlistByFS("/namelist.txt")
+			if err != nil {
+				return err
+			}
+		}
+	}
+	if c.Passive && c.Active {
+		return errors.New("Active enumeration cannot be performed without DNS resolution")
+	}
+	if c.Alterations {
+		if len(c.AltWordlist) == 0 {
+			c.AltWordlist, err = getWordlistByFS("/alterations.txt")
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	c.Wordlist, err = wordlist.ExpandMaskWordlist(c.Wordlist)
+	if err != nil {
+		return err
+	}
+
+	c.AltWordlist, err = wordlist.ExpandMaskWordlist(c.AltWordlist)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+// LoadSettings parses settings from an .ini file and assigns them to the Config.
+func (c *Config) LoadSettings(path string) error {
+	cfg, err := ini.LoadSources(ini.LoadOptions{
+		Insensitive:  true,
+		AllowShadows: true,
+	}, path)
+	if err != nil {
+		return fmt.Errorf("Failed to load the configuration file: %v", err)
+	}
+	// Get the easy ones out of the way using mapping
+	if err = cfg.MapTo(c); err != nil {
+		return fmt.Errorf("Error mapping configuration settings to internal values: %v", err)
+	}
+	// Attempt to load a special mode of operation specified by the user
+	if cfg.Section(ini.DEFAULT_SECTION).HasKey("mode") {
+		mode := cfg.Section(ini.DEFAULT_SECTION).Key("mode").String()
+
+		if mode == "passive" {
+			c.Passive = true
+		} else if mode == "active" {
+			c.Active = true
+		}
+	}
+
+	loads := []func(cfg *ini.File) error{
+		c.loadResolverSettings,
+		c.loadScopeSettings,
+		c.loadAlterationSettings,
+		c.loadBruteForceSettings,
+		c.loadDatabaseSettings,
+		c.loadDataSourceSettings,
+	}
+	for _, load := range loads {
+		if err := load(cfg); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// AcquireConfig populates the Config struct provided by the config argument.
+func AcquireConfig(dir, file string, config *Config) error {
+	var err error
+
+	if file != "" {
+		err = config.LoadSettings(file)
+		if err == nil {
+			return nil
+		}
+	}
+	// Attempt to obtain the configuration file from the output directory
+	if dir = OutputDirectory(dir); dir != "" {
+		if finfo, err := os.Stat(dir); !os.IsNotExist(err) && finfo.IsDir() {
+			file := filepath.Join(dir, "config.ini")
+
+			err = config.LoadSettings(file)
+			if err == nil {
+				return nil
+			}
+		}
+	}
+	return err
+}
+
+// OutputDirectory returns the file path of the Amass output directory. A suitable
+// path provided will be used as the output directory instead.
+func OutputDirectory(dir ...string) string {
+	if len(dir) > 0 && dir[0] != "" {
+		return dir[0]
+	}
+
+	if path, err := os.UserConfigDir(); err == nil {
+		return filepath.Join(path, outputDirectoryName)
+	}
+
+	return ""
+}
+
+// GetListFromFile reads a wordlist text or gzip file and returns the slice of words.
+func GetListFromFile(path string) ([]string, error) {
+	var reader io.Reader
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("Error opening the file %s: %v", path, err)
+	}
+	defer file.Close()
+	reader = file
+
+	// We need to determine if this is a gzipped file or a plain text file, so we
+	// first read the first 512 bytes to pass them down to http.DetectContentType
+	// for mime detection. The file is rewinded before passing it along to the
+	// next reader
+	head := make([]byte, 512)
+	if _, err = file.Read(head); err != nil {
+		return nil, fmt.Errorf("Error reading the first 512 bytes from %s: %s", path, err)
+	}
+	if _, err = file.Seek(0, 0); err != nil {
+		return nil, fmt.Errorf("Error rewinding the file %s: %s", path, err)
+	}
+
+	// Read the file as gzip if it's actually compressed
+	if mt := http.DetectContentType(head); mt == "application/gzip" || mt == "application/x-gzip" {
+		gzReader, err := gzip.NewReader(file)
+		if err != nil {
+			return nil, fmt.Errorf("Error gz-reading the file %s: %v", path, err)
+		}
+		defer gzReader.Close()
+		reader = gzReader
+	}
+
+	s, err := getWordList(reader)
+	return s, err
+}
+
+func getWordlistByURL(url string) ([]string, error) {
+	page, err := amasshttp.RequestWebPage(url, nil, nil, "", "")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to obtain the wordlist at %s: %v", url, err)
+	}
+	return getWordList(strings.NewReader(page))
+}
+
+func getWordlistByFS(path string) ([]string, error) {
+	fsOnce.Do(openTheFS)
+
+	content, err := StatikFS.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to obtain the embedded wordlist: %s: %v", path, err)
+	}
+	return getWordList(content)
+}
+
+func getWordList(reader io.Reader) ([]string, error) {
+	var words []string
+
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		// Get the next word in the list
+		w := strings.TrimSpace(scanner.Text())
+		if err := scanner.Err(); err == nil && w != "" {
+			words = append(words, w)
+		}
+	}
+	return stringset.Deduplicate(words), nil
+}
+
+func uniqueIntAppend(s []int, e string) []int {
+	if a1, err := strconv.Atoi(e); err == nil {
+		var found bool
+
+		for _, a2 := range s {
+			if a1 == a2 {
+				found = true
+				break
+			}
+		}
+		if !found {
+			s = append(s, a1)
+		}
+	}
+	return s
+}
